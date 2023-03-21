04432fc387161a522b9c57b4fb2b35c6fc8e97b6
Parasssk <115403681+Parasssk@users.noreply.github.com>
2023-03-21 18:16:53 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -49,4 +49,48 @@ int main() {
    }
 
    return 0;
+   }
+         break;
+
+      case '%':
+         printf("Enter the first number: ");
+         scanf("%lf", &num1);
+
+         printf("Enter the second number: ");
+         scanf("%lf", &num2);
+
+         if (fmod(num1, num2) == 0) {
+            printf("%.2lf is divisible by %.2lf", num1, num2);
+         } else {
+            printf("%.2lf is not divisible by %.2lf", num1, num2);
+         }
+         break;
+
+      case '^':
+         printf("Enter the base: ");
+         scanf("%lf", &num1);
+
+         printf("Enter the exponent: ");
+         scanf("%lf", &num2);
+
+         result = pow(num1, num2);
+         printf("%.2lf ^ %.2lf = %.2lf", num1, num2, result);
+         break;
+
+      case 's':
+         printf("Enter the number: ");
+         scanf("%lf", &num1);
+
+         if (num1 < 0) {
+            printf("Error! Square root of a negative number is not allowed.");
+         } else {
+            result = sqrt(num1);
+            printf("Square root of %.2lf = %.2lf", num1, result);
+         }
+         break;
+
+      default:
+         printf("Error! Invalid operator.");
+   }
+
 }
