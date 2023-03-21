aa496430f9346900132bc4c2d27462ddca7f4c15
Parasssk <115403681+Parasssk@users.noreply.github.com>
2023-03-21 18:22:43 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -56,7 +56,7 @@ int main() {
          printf("Enter the first number: ");
          scanf("%lf", &num1);
 
-         printf("Enter the second number: ");
+         printf("Enter the second numberr: ");
          scanf("%lf", &num2);
 
          if (fmod(num1, num2) == 0) {
